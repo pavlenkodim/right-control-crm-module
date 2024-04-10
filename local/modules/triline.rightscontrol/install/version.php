4ce714be3157e1d74d819cb5ce174aab8f05897b
Admin3 Triline <admin3@triline.kz>
2024-04-10 16:51:20 +0500

    Add basic structure to module, add functions installations and uninstallations

---
+++ local/modules/triline.rightscontrol/install/version.php
@@ -0,0 +1,5 @@
+<?php
+$arModuleVersion = array(
+    "VERSION" => "0.1.0",
+    "VERSION_DATE" => "2024-04-10 14:50:00"
+);
---
+++ local/modules/triline.rightscontrol/lang/ru/install/index.php
@@ -0,0 +1,21 @@
+<?php
+$MESS["TRILINE_RIGHTSCONTROLCRM_MODULE_NAME"] = "Разграничение прав CRM";
+$MESS["TRILINE_RIGHTSCONTROLCRM_MODULE_DESC"] = "Модуль позволяет настраивать видимость событий в Timeline в зависимости от роли пользователя, обеспечивая гибкое управление конфиденциальностью и общим доступом к данным CRM.";
+$MESS["TRILINE_RIGHTSCONTROLCRM_PARTNER_NAME"] = "Triline";
+$MESS["TRILINE_RIGHTSCONTROLCRM_PARTNER_URI"] = "https://triline.kz/";
+
+$MESS["TRILINE_RIGHTSCONTROLCRM_DENIED"] = "Доступ закрыт";
+$MESS["TRILINE_RIGHTSCONTROLCRM_READ_COMPONENT"] = "Доступ к компонентам";
+$MESS["TRILINE_RIGHTSCONTROLCRM_WRITE_SETTINGS"] = "Изменение настроек модуля";
+$MESS["TRILINE_RIGHTSCONTROLCRM_FULL"] = "Полный доступ";
+
+$MESS["TRILINE_RIGHTSCONTROLCRM_INSTALL_TITLE"] = "Установка модуля";
+$MESS["TRILINE_RIGHTSCONTROLCRM_UNINSTALL_TITLE"] = "Удаление модуля" ;
+$MESS["TRILINE_RIGHTSCONTROLCRM_INSTALL_ERROR_VERSION"] = "Версия главного модуля ниже 14. Не поддерживается технология D7, необходимая модулю. Пожалуйста обновите систему.";
+
+#работа с .settings.php
+$MESS["TRILINE_RIGHTSCONTROLCRM_INSTALL_COUNT"] = "Количество установок модуля: ";
+$MESS["TRILINE_RIGHTSCONTROLCRM_UNINSTALL_COUNT"] = "Количество удалений модуля: ";
+
+$MESS["TRILINE_RIGHTSCONTROLCRM_NO_CACHE"] = 'Внимание, на сайте выключено кеширование!<br>Возможно замедление в работе модуля.';
+#работа с .settings.php
